0c74de5792d0a44fb71e19f6f2d1aaba3199fd47
seqs <seqs@consenbus.org>
2018-03-02 23:34:12 +0800

    add account selector

--- src/containers/Home/Tab/Receive.js
+++ src/containers/Home/Tab/Receive.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import { observer, inject } from "mobx-react";
+import _map from "lodash/map";
 import QRCode from "qrcode.react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import Snackbar from "material-ui/Snackbar";
+import Button from "material-ui/Button";
+import Menu, { MenuItem } from "material-ui/Menu";
+import DownIcon from "material-ui-icons/KeyboardArrowDown";
+import teal from "material-ui/colors/teal";
 
 import Layout from "../_Layout";
 import Header from "./_Header";
@@ -10,10 +16,11 @@ class Receive extends Component {
   state = {
     value: "",
     copied: false,
-    visible: false
+    visible: false,
+    anchorEl: null
   };
 
-  handleClose = (event, reason) => {
+  handleSnackbarClose = (event, reason) => {
     if (reason === "clickaway") {
       return;
     }
@@ -21,7 +28,21 @@ class Receive extends Component {
     this.setState({ visible: false });
   };
 
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  handleChangeAccount = account => () => {
+    this.props.account.changeCurrentAccount(account);
+    this.setState({ anchorEl: null });
+  };
+
   render() {
+    const account = this.props.account.currentAccount;
     return (
       <Layout active="receive">
         <Header title="Receive" />
@@ -33,25 +54,59 @@ class Receive extends Component {
             marginTop: "50px"
           }}
         >
-          <QRCode value="http://facebook.github.io/react/" size={256} />
+          <QRCode value={account.account || "null"} size={256} />
 
-          <CopyToClipboard
-            text={this.state.value}
-            onCopy={() => {
-              this.setState({ copied: true, visible: true });
-              window.setTimeout(() => this.setState({ visible: false }), 2000);
-            }}
-          >
-            <span className="ellipsis" style={{ marginTop: "30px" }}>
-              xrb_3hnoeimynrdeahtfe5np78cmzu1pomh8xsm8abosze6ezyx1wwz8b8te4j5x
-            </span>
-          </CopyToClipboard>
+          {/* account selector */}
+          <div style={{ marginTop: "30px" }}>
+            <Button
+              aria-owns={this.state.anchorEl ? "simple-menu" : null}
+              aria-haspopup="true"
+              onClick={this.handleClick}
+            >
+              {account.name} <DownIcon />
+            </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={this.state.anchorEl}
+              open={Boolean(this.state.anchorEl)}
+              onClose={this.handleClose}
+            >
+              {_map(this.props.account.accounts, a => {
+                return (
+                  <MenuItem
+                    key={a.account}
+                    onClick={this.handleChangeAccount(a.account)}
+                  >
+                    {a.name || "null"}
+                  </MenuItem>
+                );
+              })}
+            </Menu>
+          </div>
+
+          {/* account address */}
+          <div style={{ marginTop: "10px" }}>
+            <CopyToClipboard
+              text={account.account}
+              onCopy={() => {
+                this.setState({ copied: true, visible: true });
+                window.setTimeout(
+                  () => this.setState({ visible: false }),
+                  2000
+                );
+              }}
+            >
+              <span className="ellipsis" style={{ color: teal["A700"] }}>
+                {account.account || "null"}
+              </span>
+            </CopyToClipboard>
+          </div>
 
           {this.state.visible && (
             <Snackbar
               anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
               open={this.state.visible}
-              onClose={this.handleClose}
+              onClose={this.handleSnackbarClose}
               SnackbarContentProps={{
                 "aria-describedby": "message-id"
               }}
@@ -64,4 +119,4 @@ class Receive extends Component {
   }
 }
 
-export default Receive;
+export default inject("account")(observer(Receive));
