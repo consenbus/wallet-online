cd7366ab31edcbb8ddf7e36f54542a50c3e5a985
seqs <seqs@consenbus.org>
2018-03-02 23:27:07 +0800

    remove unused vars

--- src/containers/Home/Tab/_Header.js
+++ src/containers/Home/Tab/_Header.js
@@ -4,7 +4,7 @@ import { withStyles } from "material-ui/styles";
 import AppBar from "material-ui/AppBar";
 import Toolbar from "material-ui/Toolbar";
 import Typography from "material-ui/Typography";
-import Button from "material-ui/Button";
+// import Button from "material-ui/Button";
 import IconButton from "material-ui/IconButton";
 import LogoIcon from "mdi-material-ui/Blur";
 
